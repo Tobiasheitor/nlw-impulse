f9c1cedfc829d345b20d1b0f4f888e8f56f24504
tgoetter <Gittobi471dafbmkot>
2022-05-02 20:20:27 -0300

    Day 1: Use of Tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,18 @@
+
 interface ButtonProps {
   text?: string;
 }
 
 function Button(props: ButtonProps) {
-  return <button>{props.text ?? 'Button'}</button>
+  return <button className="bg-[#8257e6] px-4 rounded text-violet-200 hover:bg-violet-700 transition-colors">{props.text ?? 'Button'}</button>
 }
 
 function App() {
-  return <>
-    <Button text="Enviar" />
+  return <div className="flex gap-2">
+    <Button text="Send" />
     <Button text="Ok" />
     <Button />
-  </>
+  </div>
 }
 
 export default App
