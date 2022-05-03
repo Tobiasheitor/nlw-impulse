6efe48522e3c7cb2b2780dfdf6c1324454e4afad
tgoetter <Gittobi471dafbmkot>
2022-05-02 21:39:06 -0300

    Day 1: Finish Widget component

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -1,5 +1,13 @@
 import { ChatTeardropDots} from 'phosphor-react';
 
 export function Widget() {
-    return <ChatTeardropDots />
+    return (
+        <div className='absolute bottom-5 right-5'>
+            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+                <ChatTeardropDots className='w-6 h-6 text-white'/>
+
+                <span className='max-w-0 overflow-hidden group-hover:max-w-lg transition-all duration-700 ease-linear'>Feedback</span>
+            </button>
+        </div>
+    )
 }
