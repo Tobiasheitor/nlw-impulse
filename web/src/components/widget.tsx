4802a0684e43e138b8f86ed501be106dee2994ec
tgoetter <Gittobi471dafbmkot>
2022-05-02 21:47:05 -0300

    Day 1: Create de feature toggle for feedback window

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -1,9 +1,18 @@
 import { ChatTeardropDots} from 'phosphor-react';
+import { useState } from 'react';
 
 export function Widget() {
+    const [isFeedbackOpen, setIsFeedbackOpen] = useState(false)
+
+    function toggleFeedBackVisibility() {
+        setIsFeedbackOpen(!isFeedbackOpen);
+    }
+
     return (
         <div className='absolute bottom-5 right-5'>
-            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+            { isFeedbackOpen && <p>Hello world</p>}
+            
+            <button onClick={toggleFeedBackVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6 text-white'/>
 
                 <span className='max-w-0 overflow-hidden group-hover:max-w-lg transition-all duration-700 ease-linear'>Feedback</span>
