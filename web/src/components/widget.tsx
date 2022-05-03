4a56f109caa5642126569c8c51d052c204e5c5ce
tgoetter <Gittobi471dafbmkot>
2022-05-02 21:54:50 -0300

    Day 1: Add headlessui lib for Popover component

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -1,22 +1,16 @@
 import { ChatTeardropDots} from 'phosphor-react';
-import { useState } from 'react';
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-    const [isFeedbackOpen, setIsFeedbackOpen] = useState(false)
-
-    function toggleFeedBackVisibility() {
-        setIsFeedbackOpen(!isFeedbackOpen);
-    }
-
     return (
-        <div className='absolute bottom-5 right-5'>
-            { isFeedbackOpen && <p>Hello world</p>}
-            
-            <button onClick={toggleFeedBackVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+        <Popover className='absolute bottom-5 right-5'>
+            <Popover.Panel>Hello World</Popover.Panel>
+
+            <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6 text-white'/>
 
                 <span className='max-w-0 overflow-hidden group-hover:max-w-lg transition-all duration-700 ease-linear'>Feedback</span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
