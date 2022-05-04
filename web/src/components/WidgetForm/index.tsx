0fa8261617460b322b7b0f2f034da562dc8b50c1
tgoetter <Gittobi471dafbmkot>
2022-05-04 11:34:50 -0300

    Day 2: Implement the restart of feedback and improve de feedback content

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 
-import { CloseButton } from "../CloseButton";
-
 import bugImgUrl from '../../assets/bug.svg';
 import ideaImgUrl from '../../assets/idea.svg';
 import thoughtImgUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -36,12 +35,19 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+    function handleRestartFeedback() {
+        setFeedbackType(null)
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
-                <p>Hello World</p>
+                <FeedbackContentStep
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
             )}
 
             <footer className="text-xs text-neutral-400">
