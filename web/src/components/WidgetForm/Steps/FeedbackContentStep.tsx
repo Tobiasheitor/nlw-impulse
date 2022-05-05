8bac6418295592b4a2a4c1c23d69ca1b0bbf634a
tgoetter <Gittobi471dafbmkot>
2022-05-05 15:54:29 -0300

    Day 3: Setup of server project and configs

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -2,7 +2,7 @@ import { ArrowLeft } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
-import { ScreenshotButton } from "../ScreenShotButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType,
