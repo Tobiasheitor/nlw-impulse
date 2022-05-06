65641ecfb7624a47325e95b086676d28075ea1bf
tgoetter <Gittobi471dafbmkot>
2022-05-06 18:39:32 -0300

    Day 3: Adding test dependencies and create tests

--- server/README.md
+++ server/README.md
@@ -7,3 +7,17 @@ Run the prisma studio
 ```
 npx prisma studio
 ```
+For test
+```
+npm install jest -D
+npm install ts-node -D -- for jest understand files in format typescript
+```
+For run tests
+```
+npm i -D @swc/jest
+npm i @types/jest -D -- for typescript declarations
+```
+Run a test
+```
+npm run test
+```
---
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -0,0 +1,38 @@
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+const submitFeedback = new SubmitFeedbackUseCase(
+    { create: async () => {}},
+    { sendMail: async () => {}}
+)
+
+describe('Submit feedback', () => {
+    it('should be able to submit a feedback', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64Lol'
+        })).resolves.not.toThrow();
+    });
+
+    it('should not be able to submit feedback without type', async () => {
+        await expect(submitFeedback.execute({
+            type: '',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64Lol'
+        })).rejects.toThrow();
+    });
+    it('should not be able to submit feedback without comment', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: '',
+            screenshot: 'data:image/png;base64Lol'
+        })).rejects.toThrow();
+    });
+    it('should not be able to submit a feedback with an invalid screenshot', async () => {
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'invalid format'
+        })).rejects.toThrow();
+    });
+});
